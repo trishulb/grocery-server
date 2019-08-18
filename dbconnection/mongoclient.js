6baed87431390f2caa90ce21965f8f7b34c6f040
Bhuyar, Trishul <Trishul.Bhuyar@cdk.com>
2019-08-18 14:14:22 +0530

    service file encrypt

--- dbconnection/mongoclient.js
+++ dbconnection/mongoclient.js
@@ -1,5 +1,5 @@
 const mongoose    =   require("mongoose");
-mongoose.connect('mongodb://'+'mongo'+':27017/grocerydb', { useNewUrlParser: true }).then(() => {
+mongoose.connect('mongodb://'+process.env.MONGO_HOST+':27017/grocerydb', { useNewUrlParser: true }).then(() => {
     console.log("Connected to Database");
 }).catch((err) => {
     console.log("Not Connected to Database ERROR! ", err);
